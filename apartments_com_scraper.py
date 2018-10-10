b4c17a923d491bf1789e2361e5fa0633fee27ca4
[Laura Lai] <[lauralai2.0@gmail.com]>
2018-10-10 15:36:11 -0400

    Convert fee results to JSON output

--- apartments_com_scraper.py
+++ apartments_com_scraper.py
@@ -6,15 +6,19 @@ Created on Fri Sep 14 14:43:23 2018
 """
 
 from bs4 import BeautifulSoup as bs
+import json
+import pandas as pd
 import re
 import requests
 
-# Mount Vernon Plaza URL
+### Apartments URLs
+# TODO: Store in DB
 url = 'https://www.apartments.com/mount-vernon-plaza-washington-dc/m4229lh/'
 url2 = 'https://www.apartments.com/mass-court-washington-dc/ghlk6jw/'
 
-# Get the data
+all_urls = [url, url2]
 
+### Get the data
 # Headers passed by browser, makes the request work
 req_headers = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
@@ -27,7 +31,7 @@ req_headers = {
 r = requests.get(url2, headers=req_headers)
 r.status_code
 
-# Functions
+### Functions
 def make_fee_dict(raw_text):
     clean_split = [x for x in raw_text.split('\n\n') if re.search('\$', x)]
     clean_dict = {}
@@ -36,15 +40,23 @@ def make_fee_dict(raw_text):
         stripped = re.split('\n', stripped)
         clean_dict[stripped[0]] = stripped[1]
     return clean_dict
-        
+
+'''
+Let's make some soup
+'''     
 # Create soup object from request
 soup = bs(r.content, 'html.parser')
-
 # Name of Apartment
 apt_name = soup.find(class_='propertyName').get_text().strip()
 
+''' 
+Collect Data of Interest
+- Recurring Expenses
+- One-Time Expenses
+- Amenities
+'''
 # Gather Amenities
-amenities = soup.find(id='amenitiesSection')
+amenities = soup.find(id='amenitiesSection').get_text()
 
 # Gather Fees
 fees = soup.find(id='feesSection')
@@ -52,11 +64,23 @@ fees = soup.find(id='feesSection')
 # List of recurring fees
 recurring = fees.find(class_='monthlyFees').get_text()
 recurring_dict = make_fee_dict(recurring)
+
 # List of one time fees
 onetime = fees.find(class_='oneTimeFees').get_text()
 onetime_dict = make_fee_dict(onetime)
 
-print('Property: ' + apt_name)
-print(recurring_dict)
-print(onetime_dict)
+'''
+Putting it all together (JSONify)
+'''
+result_dict = {'property-name': apt_name,
+          'one-time-expenses': onetime_dict,
+          'recurring-expenses': recurring_dict
+          }
+
+result = json.dumps(result_dict, indent=2)
+
+'''
+Print the results to console
+'''
+print(result)
 
